2e744815b9a0b39c805d51818fd24f79a1383b9d
nicklausw <n@nicklausw.com>
2022-02-09 17:49:02 -0500

    make things work

--- src/main.ts
+++ src/main.ts
@@ -32,23 +32,22 @@ async function handleWord(thisword: string, wordTable: string, serverSchema: str
     if(word.length > 50 || word === "") {
       return;
     }
-    const usesQuery = await query({sql: "select uses from " + serverSchema + wordTable + " where word=\'" + word + "\';"});
+    const usesQuery: any = await query({sql: "select uses from " + serverSchema + wordTable + " where word=\'" + word + "\';"});
     var uses: Number = 1;
     try {
-      // @ts-ignore
       uses = usesQuery["results"][0]["uses"] + 1;
     } catch { }
 
     await query({sql: "insert into " + serverSchema + wordTable + " (word, uses) values (" + "\'" + word + "\', " + uses + ") on duplicate key update uses = " + uses + ";"});
 }
 
-async function handleMessage(message: Message, isNew: boolean) {
+async function handleMessage(message: Message, runCommands: boolean) {
   var wordArray: Array<string>;
   var serverSchema: string = "s" + message.guild!.id + ".";
 
-  if(isNew) console.log("processing message from " + message.author.id + "...");
+  if(runCommands) console.log("processing message from " + message.author.id + "...");
 
-  if(message.content.toLowerCase().startsWith("indexchannels ") && isNew) {
+  if(message.content.toLowerCase().startsWith("indexchannels ") && runCommands) {
     for(var c = 0; c < message.mentions.channels.size; c++) {
       var channel : TextBasedChannel = message.mentions.channels.get(message.mentions.channels.keyAt(c)!)!;
       console.log("indexing " + channel.id + "...");
@@ -58,7 +57,7 @@ async function handleMessage(message: Message, isNew: boolean) {
         messageList.push(thisMessage);
       })
       do {
-        await handleMessage(messageList[0], false);
+        await newMessage(messageList[0], false);
         messageList.shift();
       } while(messageList[0] !== undefined);
       console.log("indexed " + channel.id + ".");
@@ -67,7 +66,7 @@ async function handleMessage(message: Message, isNew: boolean) {
     return;
   }
 
-  if(message.content.toLowerCase().startsWith("favoriteword ") && isNew) {
+  if(message.content.toLowerCase().startsWith("favoriteword ") && runCommands) {
     var person: string = v.trim(message.content.toLowerCase().split("favoriteword ")[1], "<@!>");
     if(person === undefined) {
       message.reply("format: favoriteword (@ person)");
@@ -77,21 +76,34 @@ async function handleMessage(message: Message, isNew: boolean) {
     var tempTable: string = serverSchema + "temp_table_" + Math.round(Math.random() * 10000);
     try {
       await query({sql: "create table " + tempTable + " as select * from " + thisWordTable + " where length(word) > 5;"});
+      const results: any = await query({sql: "select * from " + tempTable + " where uses = (select max(uses) from " + tempTable + ");"});
+      var uses = 0;
+      var words = new Array<string>();
+      results.forEach(e => {
+        words.push(e["word"]);
+        uses = e["uses"];
+      });
+      if(words.length === 1) {
+        message.reply("Favorite word is " + words[0] + " with " + uses + " use" + (uses > 1 ? "s" : "") + ".");
+      } else {
+        var response: string = "Favorite words are ";
+        for(var c = 0; c < words.length; c++) {
+          response += words[c];
+          if(c === words.length - 2) response += " and "
+          else if(c !== words.length - 1) response += ", "
+        }
+        response += " with " + uses + " uses.";
+        message.reply(response);
+      }
     } catch (error) {
       message.reply("came up empty on that one.");
       return;
     }
-    const results = await query({sql: "select * from " + tempTable + " where uses = (select max(uses) from " + tempTable + ");"});
-    //@ts-ignore
-    var uses: number = results["results"][0]["uses"];
-    //@ts-ignore
-    var word: string = results["results"][0]["word"];
-    message.reply("Favorite word is " + word + " with " + uses + " use" + (uses > 1 ? "s" : "") + ".");
     await query({sql: "drop table " + tempTable + ";"});
     return;
   }
 
-  if(message.content.toLowerCase().startsWith("wordcount ") && isNew) {
+  if(message.content.toLowerCase().startsWith("wordcount ") && runCommands) {
     var parameters: Array<string> = message.content.toLowerCase().split(" ");
     if(parameters.length != 3) {
       message.reply("format: wordcount (@ person) (single word)");
@@ -99,11 +111,10 @@ async function handleMessage(message: Message, isNew: boolean) {
     }
     var person: string = v.trim(parameters[1], "<@!>");
     var word: string = parameters[2].replace("\'", "\'\'");
-    var thisWordTable: string = "u" + person + "_words";
+    var thisWordTable: string = serverSchema + "u" + person + "_words";
     try {
-      const results = await query({sql: "select * from " + thisWordTable + " where word = \'" + word + "\';"});
-      //@ts-ignore
-      var uses: number = results["results"][0]["uses"];
+      const results: any = await query({sql: "select * from " + thisWordTable + " where word = \'" + word + "\';"});
+      var uses: number = results[0]["uses"];
       message.reply("User has said \"" + word + "\" " + uses + " time" + (uses > 1 ? "s" : "") + ".");
     } catch {
       message.reply("came up empty on that one.");
@@ -125,7 +136,13 @@ async function handleMessage(message: Message, isNew: boolean) {
     words.shift();
   } while(words[0] !== undefined)
 
-  if(isNew) console.log("processed message from " + message.author.id + ".");
+  if(runCommands) console.log("processed message from " + message.author.id + ".");
+}
+
+async function newMessage(message: Message, runCommands: boolean) {
+  await query({sql: "create schema if not exists s" + message.guild!.id + ";"});
+  await query({sql: "create table if not exists s" + message.guild!.id + ".users(id varchar(50));"});
+  await handleMessage(message, runCommands);
 }
 
 export const client = new Client({
@@ -178,10 +195,7 @@ client.on("interactionCreate", (interaction: Interaction) => {
 client.on("messageCreate", (message: Message) => {
   if(message.author.id === client.user!.id) return;
 
-  query({sql: "create schema if not exists s" + message.guild!.id + ";"}).then(x => {
-    query({sql: "create table if not exists s" + message.guild!.id + ".users(id varchar(50));"});
-    handleMessage(message, true);
-  });
+  newMessage(message, true);
 });
 
 async function run() {
